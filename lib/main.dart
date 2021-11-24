db02d7dd2710dd6bdd33989cd860a5d6e0d0db8e
Tawanrung <shatsuku@gmail.com>
2021-11-24 23:48:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'my first titele'),
+      home: const MyHomePage(title: 'my first title'),
     );
   }
 }
